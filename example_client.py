5576180415fbaee29e8010fe79b2df6d983d254b
root <admin@vpntech.net>
2020-01-19 15:31:23 -0500

    working example script

--- example_client.py
+++ example_client.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import os, sys, parse_process_accounting
+import os, sys, json, parse_process_accounting
 
-Stats = parse_process_accounting.getStats()
-print(Stats)
+print(json.dumps(parse_process_accounting.getStats()))
